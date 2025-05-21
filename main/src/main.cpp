85551e018e6bcdd3e46263b731443de809fd1298
DarkSoulEngineer <milchiscatalin@gmail.com>
2025-05-21 20:20:27 +0300

    v0

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -4,9 +4,13 @@
 extern "C" {
     #include "ap_scanner.h"   // Handle AP scanning
     #include "command_line.h" // Handle command line input
+
+    #include "sniffer.h"      // Handle sniffer : TO BE IMPLEMENTED
 }
 
 
 void app_main() {             // Start of main function
     read_user_input(1);       // Call the function to read user input
+    
+    // wifictl_sniffer_start(11);
 }
